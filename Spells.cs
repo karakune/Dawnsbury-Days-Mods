ab43a227199af15ab70f5f1ce8c21b0428f751eb
karakune <hugopelletier783@gmail.com>
2025-06-13 00:20:15 -0400

    Can pick class and choose first hex (hex spells not functional)

---
+++ Spells.cs
@@ -0,0 +1,51 @@
+using Dawnsbury.Core.CharacterBuilder.FeatsDb.Spellbook;
+using Dawnsbury.Core.CharacterBuilder.Spellcasting;
+using Dawnsbury.Core.Mechanics.Core;
+using Dawnsbury.Core.Mechanics.Enumerations;
+using Dawnsbury.Core.Mechanics.Targeting;
+using Dawnsbury.Display.Illustrations;
+using Dawnsbury.Modding;
+
+namespace Dawnsbury.Mods.Classes.Witch;
+
+public static class WitchSpells
+{
+	public static Trait THex = ModManager.RegisterTrait("Hex");
+
+	public static SpellId PatronsPuppet = ModManager.RegisterNewSpell("PatronsPuppet", 1,
+		(spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+		{
+			return Spells.CreateModern(new ModdedIllustration("AcidicBurstAssets/AcidicBurst.png"), "Patron's Puppet", [Trait.Focus, THex, WitchLoader.TWitch],
+					"At your unspoken plea, your patron temporarily assumes control over your familiar.",
+					"You Command your familiar, allowing it to take its normal actions this turn.",
+					Target.Self(), spellLevel, null);
+		});
+	public static SpellId PhaseFamiliar = ModManager.RegisterNewSpell("PhaseFamiliar", 1,
+		(spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+		{
+			return Spells.CreateModern(new ModdedIllustration("AcidicBurstAssets/AcidicBurst.png"), "Phase Familiar", [Trait.Focus, THex, Trait.Manipulate, WitchLoader.TWitch],
+				"Your patron momentarily recalls your familiar to the ether, shifting it from its solid, physical form into a ghostly version of itself.",
+				"Against the triggering damage, your familiar gains resistance 5 to all damage and is immune to precision damage.",
+				Target.Self(), spellLevel, null);
+		});
+	
+	public static SpellId ShroudOfNight = ModManager.RegisterNewSpell("ShroudOfNight", 0, ((spellId, spellcaster, spellLevel, inCombat, spellInformation) =>
+	{
+		return Spells.CreateModern(new ModdedIllustration("AcidicBurstAssets/AcidicBurst.png"), "Shroud of Night", [Trait.Focus, Trait.Cantrip, Trait.Darkness, Trait.Manipulate, THex, WitchLoader.TWitch],
+				"Your patron blankets the target's eyes in darkness.",
+				"Success The target is unaffected. Failure All creatures are concealed to it.",
+				Target.RangedCreature(6), spellLevel, SpellSavingThrow.Standard(Defense.Will))
+			// .WithSoundEffect(ModManager.RegisterNewSoundEffect("AcidicBurstAssets/AcidicBurstSfx.mp3"))
+			.WithEffectOnEachTarget((async (spell, caster, target, result) =>
+			{
+				if (result >= CheckResult.Success)
+					return;
+					
+				// target.AddQEffect(new QEffect("Murky Darkness", "All creatures are concealed to you", ExpirationCondition.ExpiresAtEndOfSourcesTurn))
+					
+				// await CommonSpellEffects.DealBasicDamage(spell, caster, target, result, (spellLevel * 2) + "d6", DamageKind.Acid);
+				// await CommonConditionEffects.RollAgainstConfusion()
+			}));
+	}));
+	
+}
---
+++ WitchLoader.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dawnsbury.Audio;
+using Dawnsbury.Auxiliary;
+using Dawnsbury.Campaign.LongTerm;
+using Dawnsbury.Core;
+using Dawnsbury.Core.CharacterBuilder;
+using Dawnsbury.Core.CharacterBuilder.AbilityScores;
+using Dawnsbury.Core.CharacterBuilder.Feats;
+using Dawnsbury.Core.CharacterBuilder.FeatsDb.Common;
+using Dawnsbury.Core.CharacterBuilder.FeatsDb.Spellbook;
+using Dawnsbury.Core.CharacterBuilder.Selections.Options;
+using Dawnsbury.Core.CharacterBuilder.Selections.Selected;
+using Dawnsbury.Core.CharacterBuilder.Spellcasting;
+using Dawnsbury.Core.CombatActions;
+using Dawnsbury.Core.Creatures;
+using Dawnsbury.Core.Mechanics;
+using Dawnsbury.Core.Mechanics.Core;
+using Dawnsbury.Core.Mechanics.Enumerations;
+using Dawnsbury.Core.Mechanics.Targeting;
+using Dawnsbury.Core.Mechanics.Targeting.TargetingRequirements;
+using Dawnsbury.Core.Possibilities;
+using Dawnsbury.Display.Illustrations;
+using Dawnsbury.Display.Text;
+using Dawnsbury.Modding;
+using Dawnsbury.Phases.Menus.CharacterBuilderPages;
+
+namespace Dawnsbury.Mods.Classes.Witch;
+
+public class WitchLoader
+{
+	public static Trait TWitch = ModManager.RegisterTrait("Witch", new TraitProperties("Witch", true)
+	{
+		IsClassTrait = true
+	});
+
+	public static Trait TFirstHex = ModManager.RegisterTrait("First Hex");
+
+	private static FeatName FNStarlessShadow = ModManager.RegisterFeatName("StarlessShadow", "Starless Shadow");
+	
+	[DawnsburyDaysModMainMethod]
+	public static void LoadMod()
+	{
+		foreach (var feat in CreateFeats())
+		{
+			ModManager.AddFeat(feat);
+		}
+	}
+
+	private static IEnumerable<Feat> CreateFeats()
+	{
+		List<Feat> subclasses = [
+			WitchPatronFeat.Create(FNStarlessShadow, Trait.Occult, Skill.Occultism, "")
+		];
+
+		yield return new Feat(ModManager.RegisterFeatName("FirstHexPatronsPuppet", "Patron's Puppet"),
+				null, "Gain the Patron's Puppet focus spell and a focus point.", [TFirstHex], null)
+			.WithOnSheet(sheet =>
+				sheet.AddFocusSpellAndFocusPoint(TWitch, Ability.Intelligence, WitchSpells.PatronsPuppet));
+
+		yield return new Feat(ModManager.RegisterFeatName("FirstHexPhaseFamiliar", "Phase Familiar"),
+				null, "Gain the Phase Familiar focus spell and a focus point.", [TFirstHex], null)
+			.WithOnSheet(sheet =>
+				sheet.AddFocusSpellAndFocusPoint(TWitch, Ability.Intelligence, WitchSpells.PhaseFamiliar));
+		
+		yield return new ClassSelectionFeat(ModManager.RegisterFeatName("FeatWitch", "Witch"),
+			"The Witch",
+			TWitch,
+			new EnforcedAbilityBoost(Ability.Intelligence),
+			6,
+			[Trait.Perception, Trait.Fortitude, Trait.Reflex, Trait.Simple, Trait.Unarmed, Trait.UnarmoredDefense, Trait.Spell],
+			[Trait.Will],
+			3,
+			"", subclasses)
+			.WithOnSheet(sheet =>
+			{
+				sheet.AddSelectionOption(new SingleFeatSelectionOption("FirstHex", "First Hex", -1, feat => feat.HasTrait(TFirstHex)));
+			});
+	}
+}
+
+public class WitchPatronFeat : Feat
+{
+	private WitchPatronFeat(FeatName patronName, string flavorText) 
+		: base(patronName, flavorText, "XX", new List<Trait>(), null)
+	{
+	}
+
+	public static Feat Create(FeatName patronName, Trait spellTradition, Skill skill, string flavorText)
+	{
+		return new WitchPatronFeat(patronName, flavorText)
+			.WithOnSheet(sheet =>
+			{
+				sheet.SpellTraditionsKnown.Add(spellTradition);
+				sheet.TrainInThisOrSubstitute(skill);
+				sheet.PreparedSpells.Add(WitchLoader.TWitch, new PreparedSpellSlots(Ability.Intelligence, Trait.Occult));
+				sheet.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(0, "Witch:Cantrip1"));
+				sheet.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(0, "Witch:Cantrip2"));
+				sheet.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(0, "Witch:Cantrip3"));
+				sheet.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(0, "Witch:Cantrip4"));
+				sheet.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(0, "Witch:Cantrip5"));
+				sheet.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(1, "Witch:Spell1-1"));
+				sheet.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(1, "Witch:Spell1-2"));
+				for (int i = 2; i <= 20; ++i)
+				{
+					int thisLevel = i;
+					if (thisLevel % 2 == 1)
+						sheet.AddAtLevel(thisLevel, values =>
+						{
+							int level = (thisLevel + 1) / 2;
+							values.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(level, $"Witch:Spell{level}-1"));
+							values.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(level, $"Witch:Spell{level}-2"));
+						});
+					else
+						sheet.AddAtLevel(thisLevel, values =>
+						{
+							int level = thisLevel / 2;
+							values.PreparedSpells[WitchLoader.TWitch].Slots.Add(new FreePreparedSpellSlot(level, $"Witch:Spell{level}-3"));
+						});
+				}
+				sheet.AddFeat(NightsTerror.Create(), null);
+				
+				sheet.AddAtLevel(5, values => values.SetProficiency(Trait.Fortitude, Proficiency.Expert));
+				sheet.AddAtLevel(7, values => values.SetProficiency(Trait.Spell, Proficiency.Expert));
+			});
+	}
+}
