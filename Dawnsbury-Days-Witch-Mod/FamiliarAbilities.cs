a0a4a552398a5da8107e56b28ef17101bc50228e
karakune <hugopelletier783@gmail.com>
2025-08-18 00:25:21 -0400

    Familiar of Restored Spirit complete

--- Dawnsbury-Days-Witch-Mod/FamiliarAbilities.cs
+++ Dawnsbury-Days-Witch-Mod/FamiliarAbilities.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using Dawnsbury.Core;
 using Dawnsbury.Core.CharacterBuilder.Feats;
+using Dawnsbury.Core.CombatActions;
 using Dawnsbury.Core.Mechanics;
 using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Core.Mechanics.Rules;
@@ -12,6 +14,12 @@ namespace Dawnsbury.Mods.Classes.Witch;
 public static class FamiliarAbilities
 {
 	public static FeatName FNStalkingNight = ModManager.RegisterFeatName("MasStalkingNight", "Familiar of Stalking Night");
+	public static FeatName FNRestoredSpirit = ModManager.RegisterFeatName("MasRestoredSpirit", "Familiar of Restored Spirit");
+	public static FeatName FNFreezingRime = ModManager.RegisterFeatName("MasFreezingRime", "Familiar of Freezing Rime");
+	public static FeatName FNBalancedLuck = ModManager.RegisterFeatName("MasBalancedLuck", "Familiar of Balanced Luck");
+	public static FeatName FNFlowingScript = ModManager.RegisterFeatName("MasFlowingScript", "Familiar of Flowing Script");
+	public static FeatName FNOngoingMisery = ModManager.RegisterFeatName("MasOngoingMisery", "Familiar of Ongoing Misery");
+	public static FeatName FNKeenSenses = ModManager.RegisterFeatName("MasKeenSenses", "Familiar of Keen Senses");
 	public static IEnumerable<Feat> CreateFeats()
 	{
 		var stalkingNightDisplay = MasterAbilities.CreateFamiliarDisplay("MasStalkingNight", "Familiar of Stalking Night");
@@ -26,10 +34,7 @@ public static class FamiliarAbilities
 				{
 					AfterYouExpendSpellcastingResources = (effect, action) =>
 					{
-						if (!action.HasTrait(WitchSpells.THex) && 
-						    action.HasTrait(Trait.SustainASpell) && 
-						    action.ReferencedQEffect is { ReferencedSpell: not null } &&
-						    !action.ReferencedQEffect.ReferencedSpell.HasTrait(WitchSpells.THex))
+						if (!IsCastingOrSustainingHex(action))
 							return;
 						
 						if (master.QEffects.Contains(Familiar.QDeadFamiliar))
@@ -54,5 +59,50 @@ public static class FamiliarAbilities
 				});
 			})
 			.WithOnCreature(owner => owner.AddQEffect(MasterAbilities.GetDisplayQEffect(stalkingNightDisplay.FeatName)));
+		
+		var restoredSpiritDisplay = MasterAbilities.CreateFamiliarDisplay("MasRestoredSpirit", "Familiar of Restored Spirit");
+		yield return restoredSpiritDisplay;
+		yield return new Feat(FNRestoredSpirit, null,
+				"When you Cast or Sustain a hex, one willing creature within 15 feet of your familiar gains temporary Hit Points equal to 2 + half your level, which last until the start of your next turn.", 
+				[], null)
+			.WithOnCreature((sheet, master) =>
+			{
+				var familiar = Familiar.GetFamiliar(master);
+				master.AddQEffect(new QEffect
+				{
+					AfterYouTakeAction = async (effect, action) =>
+					{
+						if (!IsCastingOrSustainingHex(action))
+							return;
+						
+						if (master.QEffects.Contains(Familiar.QDeadFamiliar))
+							return;
+						
+						// Source is master if familiar is not deployed
+						var source = familiar ?? master;
+
+						var target = await source.Battle.AskToChooseACreature(source,
+							source.Battle.AllCreatures.Where(c =>
+								c.OwningFaction.AlliedFactionOf(source.OwningFaction) && source.DistanceTo(c) <= 3), 
+							IllustrationName.Heal, "Choose an ally to receive temp hp",
+							"Choose this one", "Pass");
+
+						if (target == null)
+							return;
+						
+						var tempHp = 2 + (master.Level / 2);
+						target.GainTemporaryHP(tempHp);
+					}
+				});
+			})
+			.WithOnCreature(owner => owner.AddQEffect(MasterAbilities.GetDisplayQEffect(restoredSpiritDisplay.FeatName)));
+	}
+
+	private static bool IsCastingOrSustainingHex(CombatAction action)
+	{
+		return action.HasTrait(WitchSpells.THex) ||
+		       action.HasTrait(Trait.SustainASpell) &&
+		       action.ReferencedQEffect?.ReferencedSpell != null &&
+		       action.ReferencedQEffect.ReferencedSpell.HasTrait(WitchSpells.THex);
 	}
 }
