53bf6eedc0550335ed8a62c323a684db239e853f
karakune <hugopelletier783@gmail.com>
2025-09-29 20:45:55 -0400

    Added patron descriptions

--- Dawnsbury-Days-Witch-Mod/WitchLoader.cs
+++ Dawnsbury-Days-Witch-Mod/WitchLoader.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using Dawnsbury.Auxiliary;
 using Dawnsbury.Core.CharacterBuilder;
 using Dawnsbury.Core.CharacterBuilder.AbilityScores;
 using Dawnsbury.Core.CharacterBuilder.Feats;
+using Dawnsbury.Core.CharacterBuilder.FeatsDb;
 using Dawnsbury.Core.CharacterBuilder.FeatsDb.Spellbook;
 using Dawnsbury.Core.CharacterBuilder.Selections.Options;
 using Dawnsbury.Core.CharacterBuilder.Spellcasting;
@@ -41,10 +43,18 @@ public static class WitchLoader
 	private static IEnumerable<Feat> CreateFeats()
 	{
 		List<Feat> subclasses = [
-			WitchPatronFeat.Create(FNStarlessShadow, Trait.Occult, Skill.Occultism, WitchSpells.ShroudOfNight, SpellId.Fear, FamiliarAbilities.FNStalkingNight, ""),
-			WitchPatronFeat.Create(FNFaithsFlamekeeper, Trait.Divine, Skill.Religion, WitchSpells.StokeTheHeart, SpellId.Command, FamiliarAbilities.FNRestoredSpirit, ""),
-			WitchPatronFeat.Create(FNSpinnerOfThreads, Trait.Occult, Skill.Occultism, WitchSpells.NudgeFate, SpellId.TrueStrike, FamiliarAbilities.FNBalancedLuck, ""),
-			WitchPatronFeat.Create(FNSilenceInSnow, Trait.Primal, Skill.Nature, WitchSpells.ClingingIce, WitchSpells.GustOfWind, FamiliarAbilities.FNFreezingRime, ""),
+			WitchPatronFeat.Create(FNStarlessShadow, Trait.Occult, Skill.Occultism, WitchSpells.ShroudOfNight, SpellId.Fear, FamiliarAbilities.FNStalkingNight, 
+				"Your patron first contacted you at the witching hour, as your body lay paralyzed by sleep while your mind had yet to escape the waking world. Your patron might be a creature of the Netherworld or a long-forgotten spirit of twilight—all you remember of them are haunting eyes of moonlight, offering you power from the darkness.", 
+				"Lesson of Night's Terrors"),
+			WitchPatronFeat.Create(FNFaithsFlamekeeper, Trait.Divine, Skill.Religion, WitchSpells.StokeTheHeart, SpellId.Command, FamiliarAbilities.FNRestoredSpirit, 
+				"Your patron contacted you in a moment your willpower was close to sputtering out. Their reassuring presence was like breath and kindling bringing an ember back aflame, their magic giving you the strength to carry on and bring others to your cause. Your patron is likely a divine being like an angel or aeon acting covertly, though the possibility exists they might be a more sinister entity, using you to unknown ends.",
+				"Lesson of Fervor's Grasp"),
+			WitchPatronFeat.Create(FNSpinnerOfThreads, Trait.Occult, Skill.Occultism, WitchSpells.NudgeFate, SpellId.TrueStrike, FamiliarAbilities.FNBalancedLuck, 
+				"You met your patron in a memory of an encounter yet to come or a premonition of something long since passed, as they untangled and re-spun the tapestry of time and fate. Was your patron a norn? A herald of a deity like Pharasma, Alseta, or Grandmother Spider? Could it even be a single individual appearing at three or more points in its timeline—multiple versions of the same being, parallel threads converging on a single moment?",
+				"Lesson of Fate's Vicissitudes"),
+			WitchPatronFeat.Create(FNSilenceInSnow, Trait.Primal, Skill.Nature, WitchSpells.ClingingIce, WitchSpells.GustOfWind, FamiliarAbilities.FNFreezingRime, 
+				"Bitter cold heralded your patron's appearance, in the depths of the winter solstice or on a frozen peak at the end of the world. Your patron might be a winter hag, ice yai, or other spirit of the cold, but one thing is clear as ice—their power is not to be underestimated.",
+				"Lesson of Winter's Chill"),
 		];
 
 		yield return new Feat(ModManager.RegisterFeatName("FirstHexPatronsPuppet", "Patron's Puppet"),
@@ -77,14 +87,21 @@ public static class WitchLoader
 
 public class WitchPatronFeat : Feat
 {
-	private WitchPatronFeat(FeatName patronName, string flavorText) 
-		: base(patronName, flavorText, "XX", new List<Trait>(), null)
+	private WitchPatronFeat(FeatName patronName, string flavorText, string rulesText) 
+		: base(patronName, flavorText, rulesText, new List<Trait>(), null)
 	{
 	}
 
-	public static Feat Create(FeatName patronName, Trait spellTradition, Skill skill, SpellId hexCantrip, SpellId extraPreparableSpell, FeatName familiarAbility, string flavorText)
+	public static Feat Create(FeatName patronName, Trait spellTradition, Skill skill, SpellId hexCantrip, SpellId extraPreparableSpell, FeatName familiarAbility, string flavorText, string lessonName)
 	{
-		return new WitchPatronFeat(patronName, flavorText)
+		var featDescription = new StringBuilder();
+		featDescription.Append($"\n{{b}}Spell List:{{/b}} {spellTradition}");
+		featDescription.Append($"\n{{b}}Patron Skill:{{/b}} {skill}");
+		featDescription.Append($"\n{{b}}{lessonName}:{{/b}} You gain the {AllSpells.CreateSpellLink(hexCantrip, WitchSpells.THex)} hex cantrip and {AllSpells.CreateSpellLink(extraPreparableSpell, WitchLoader.TWitch)} is added to your preparable spell list.");
+		var famFeat = AllFeats.GetFeatByFeatName(familiarAbility);
+		featDescription.Append($"\n{{b}}{famFeat.Name}:{{/b}} {famFeat.RulesText}");
+		
+		return new WitchPatronFeat(patronName, flavorText, featDescription.ToString())
 			.WithOnSheet(sheet =>
 			{
 				sheet.GrantFeat(familiarAbility);
