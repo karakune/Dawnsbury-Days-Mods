7505177ed3ad5e7961ec14d3e2977f9f4c944756
karakune <hugopelletier783@gmail.com>
2026-03-06 07:23:45 -0500

    Added Weapons mod

---
+++ Dawnsbury.Mods.Weapons/WeaponsLoader.cs
@@ -0,0 +1,19 @@
+﻿using Dawnsbury.Core;
+using Dawnsbury.Core.Mechanics.Enumerations;
+using Dawnsbury.Core.Mechanics.Treasure;
+using Dawnsbury.Modding;
+
+namespace Dawnsbury.Mods.Weapons;
+
+public class WeaponsLoader
+{
+	[DawnsburyDaysModMainMethod]
+	public static void LoadMod()
+	{
+		ModManager.RegisterNewItemIntoTheShop("Khakkhara", name => 
+			new Item(name, IllustrationName.Quarterstaff, "Khakkhara", 0, 2, Trait.Club, 
+				Trait.Uncommon, Trait.Monk, Trait.Shove, Trait.TwoHand1d10, Trait.VersatileP, Trait.Martial, Trait.Mod)
+				.WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Bludgeoning))
+		);
+	}
+}
